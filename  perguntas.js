e8463f00a1834e14f6d2c7544cb41b38be47a159
nicolyy11 <nicoly.matos.souza@escola.pr.gov.br>
2025-05-07 09:22:39 -0300

    07.05

---
+++  perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'Geografia',
+    'Qual o pais com a maior area do mundo e qual tem a menor area do mundo?',
+    '.o pais com a maior area do mundo é a Russia, ja o menor é o varicano'
+)
+
+criaCartao(
+    'Geografia',
+    'oque é uma peninsula?',
+    'é uma porção de terra cercada por água em três lados,ligada a um continente por um istmo (uma faixa estreita de terra).'
+)
+
+criaCartao(
+    'historia',
+    'O que é monarquia constitucional?',
+    'é um sistema de governo onde o rei ou rainha é o chefe de Estado, mas seus poderes são limitados por uma constituição e um parlamento.'
+)
+
+criaCartao(
+    'historia',
+    'qual ideias voltaire apoiava?',
+    'voltaire apoiava varias ideias, algumas delas era a separação de igreja e estado (laicidade), liberdade de pensamento e expressão e tambem a liberdade religiosa.'
